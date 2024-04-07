489ce439ce9f4a3eea121a5c95fe36583e5d3772
lynnlo <tharlynnhtet.ong@gmail.com>
2024-04-07 15:30:02 -0700

    Image Optimize

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,8 +1,24 @@
 import { defineConfig } from 'vite'
-import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
+import viteImagemin from '@vheemstra/vite-plugin-imagemin'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
+import imageminPngquant from 'imagemin-pngquant'
+import imageminMozjpeg from 'imagemin-mozjpeg'
+import imageminWebp from 'imagemin-webp'
+
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), ViteImageOptimizer()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    viteImagemin({
+      plugins: { jpg: imageminMozjpeg(), png: imageminPngquant() },
+      makeWebp: {
+        plugins: {
+          jpg: imageminWebp(),
+          png: imageminWebp(),
+        },
+      },
+    }),
+  ],
 })
